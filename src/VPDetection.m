54220c8aa311b1977caa686c743729e21a2df70a
scadete <scadete@gmail.com>
2012-05-31 03:04:46 +0000

    Adicionada repeticioes ao kmeans

--- src/VPDetection.m
+++ src/VPDetection.m
@@ -4,7 +4,7 @@ function [H V] = VPDetection(lines, bwImage)
 [nP intersections] = CalculateLineIntersections(lines);
 if (nP > 0)
     nClusters = max(ceil(log(nP)),10);
-    [idx, clusters] = kmeans(intersections, nClusters);
+    [idx, clusters] = kmeans(intersections, nClusters, 'replicates', ceil(nP/2));
 end
 
 %% Vanishing point detection and selection
@@ -12,8 +12,8 @@ indirectProfit = VPIndirect(idx, clusters);
 
 %[Hdirect Vdirect] = VPDetectionIndirect(clusters, lines, bwImage);
 
-%H = 0.5 * Hindirect + 0.5 * Hdirect;
-%V = 0.5 * Vindirect + 0.5 * Vdirect;
+H = 0.5 * Hindirect + 0.5 * Hdirect;
+V = 0.5 * Vindirect + 0.5 * Vdirect;
 
 
 end
